255eaf4cac7a5aa3ab209380c22b96fbd90652f0
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-03-16 21:27:49 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+const number = 5555; 
+const singers = [
+  {name: 'Dr. Mahfuz', job: 'Singer'},
+  {name: 'Eva Rahman', job: 'Singer2'},
+  {name: 'Agun', job: 'sopno'},
+  {name: 'shuvro', job: 'pathor'}
+]
+
+
+const singerStyle= {
+  color: 'white',
+  backgroundColor: 'black',
+  border: '5px solid yellow',
+  margin: '30px',
+  width: '40%'
+}
 
 function App() {
+  const nayoks = ['Rubel', 'BappaDa', 'Kuber', 'Jashim', 'Salman Shah', 'Riyaz', 'Anwar'];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>The number is {number}</h1>
+       {
+        nayoks.map(nayok => <li>Name: {nayok}</li>)
+      }
+      {/* {
+        nayoks.map(nayok => <Person name={nayok}></Person>)
+      } */}
+
+      {
+        singers.map(singer => <Person name={singer.name}></Person>)  //name ta na dile output ashe nah
+      }
+
+      {/* <Person name={nayoks[0]} nayika="moushumi"></Person>
+      <Person name={nayoks[1]} nayika="cheka"></Person>
+      <Person name ={nayoks[2]} nayika="Kopila"></Person> */}
+      <h5>New component. YAY</h5>
+      <p id="totocompany">rock mama React mama.</p>
+      <Friend movie="Shingam" phone="01777"></Friend>
+      <Friend phone="01999"></Friend>
     </div>
   );
 }
+
+function Person(props){
+  console.log(props)
+  return (
+    <div style={singerStyle} className="person">
+      <h1>{props.name}</h1>
+      <p>{props.nayika}</p>
+  </div>
+  )
+}
+
+function Friend(props){
+  // console.log(props);
+  return (
+    <div className='container'>
+      <h3>Name: {props.movie}</h3>
+      <p>phone: {props.phone}</p>
+    </div>
+  )
+}
+
 
 export default App;
