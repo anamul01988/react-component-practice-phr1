7ec9f3e580c0b94f73b37b412585eecdb9cec952
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-03-16 21:35:46 +0600

    added counterClock and json data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+
 const number = 5555; 
 const singers = [
   {name: 'Dr. Mahfuz', job: 'Singer'},
@@ -21,6 +24,14 @@ function App() {
   const nayoks = ['Rubel', 'BappaDa', 'Kuber', 'Jashim', 'Salman Shah', 'Riyaz', 'Anwar'];
   return (
     <div className="App">
+        <Counter/>
+        <ExternalUsers/>
+
+
+
+
+
+
        <h1>The number is {number}</h1>
        {
         nayoks.map(nayok => <li>Name: {nayok}</li>)
@@ -43,6 +54,67 @@ function App() {
     </div>
   );
 }
+
+function ExternalUsers(){
+  const [users, setUsers] = useState([]); //useState a default value dewa lage
+  //  useEffect(,[])
+  //  useEffect( ()=>{},[])
+  // const myfunct = () =>{}  ==> anonymous function
+   useEffect( ()=>{
+     fetch('https://jsonplaceholder.typicode.com/users')
+     .then(rest =>rest.json())
+    //  .then(data => console.log(data))
+     .then(data => setUsers(data))
+   },[])
+
+  return(
+    <div>
+        <h1>External users</h1>
+        <p>{users.length}</p>
+        {users.map(user => <User name ={user.name} email = {user.email}></User>)}
+    </div>
+  )
+}
+function User(props){
+  console.log(props)
+  return(
+    <div style = {{border: '2px solid red', margin:'20px'}}>
+      <h3>{props.name}</h3>
+      <p>{props.email}</p>
+    </div>
+  )
+}
+
+
+
+function Counter(){
+  // const abc = useState(0);
+    // console.log(abc)
+  const [count , setCount]  = useState(0);
+  // ========  first way =========
+//  const increaseCount = () =>{
+//       const newCount = count + 1;
+//      // setCount = newCount;//ai line ta hobe na couse setCount func jeita useState dei so parameter hishebe newa lagbe
+//      setCount(newCount);
+//  }
+
+// ===================  second way ======
+ const increaseCount = () => setCount(count+1)
+ const decreaseCount = () => setCount(count-1)
+
+ return(
+   <div>
+       <h1>Count: {count}</h1>
+       <button onClick={increaseCount}>Increase</button>
+       <button onClick={decreaseCount}>Decrease</button>
+
+   </div>
+
+ )
+}
+
+
+
 
 function Person(props){
   console.log(props)
